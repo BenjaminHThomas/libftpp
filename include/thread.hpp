ec158ead5ae0d56ed55a66f6a7c16d5d54d6f889
Ben Thomas <benjamin.h.thomas1@gmail.com>
2025-04-03 11:15:56 +0200

    added class template for Thread

---
+++ include/thread.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <functional>
+#include <thread>
+
+/*
+	I'm not sure why a thread needs a wrapper,
+	it seems like unnecessary abstraction,
+	but it's a requirement of the subject.
+*/
+
+class Thread
+{
+	private:
+		const std::string _name;
+		std::function<void()> _func;
+		std::thread _thread;
+
+	public:
+		Thread(
+			const std::string & name,
+			std::function<void()> funcToExecute
+		);
+		Thread(Thread & other) = delete;
+		Thread & operator=(const &Thread) = delete;
+
+	public:
+		void start();
+		void stop();
+};
