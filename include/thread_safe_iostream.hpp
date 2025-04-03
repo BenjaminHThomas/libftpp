b38646ce712fbf4f31a7568e403288a42235426a
Ben Thomas <benjamin.h.thomas1@gmail.com>
2025-04-03 14:54:46 +0200

    added basic threadsafe IO functionality

--- include/thread_safe_iostream.hpp
+++ include/thread_safe_iostream.hpp
@@ -3,13 +3,15 @@
 #include <mutex>
 #include <string>
 #include <sstream>
+#include <iostream>
 
 class ThreadSafeIOStream
 {
 	private:
-		static std::mutex _m;
+		static std::mutex _mx;
 		std::string _prefix;
 		std::ostringstream _buffer;
+		std::istringstream _istream;
 
 	public: // ostream
 		template <typename T>
@@ -25,7 +27,7 @@ class ThreadSafeIOStream
 		template <typename T>
 		void prompt(const std::string & question, T & dest);
 		void setPrefix(const std::string & prefix);
-
+		void flush(void);
 };
 
 extern ThreadSafeIOStream threadSafeCout;
