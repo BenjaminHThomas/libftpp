2acc853b93ed1b00cb1676de972cc1d811a5113f
Ben Thomas <benjamin.h.thomas1@gmail.com>
2025-04-15 15:02:31 +0200

    fixed istream logic, made flush a private util function, moved template functions to header

--- include/thread_safe_iostream.hpp
+++ include/thread_safe_iostream.hpp
@@ -11,23 +11,34 @@ class ThreadSafeIOStream
 		static std::mutex _mx;
 		std::string _prefix;
 		std::ostringstream _buffer;
-		std::istringstream _istream;
+		void _flush(void);
 
 	public: // ostream
 		template <typename T>
-		ThreadSafeIOStream & operator <<(const T & obj);
+		ThreadSafeIOStream & operator <<(const T & obj) {
+      const std::lock_guard<std::mutex> lk(_mx);
+      _buffer << obj;
+      return *this;
+    }
 		ThreadSafeIOStream & operator <<(std::ostream& (*pf)(std::ostream&));
 
 	public: // istream
 		template <typename T>
-		ThreadSafeIOStream & operator >>(T & obj);
+		ThreadSafeIOStream & operator >>(T & obj) {
+      const std::lock_guard<std::mutex> lk(_mx);
+      std::cout << _prefix << std::flush;
+      std::cin >> obj;
+      return *this;
+    }
 		ThreadSafeIOStream & operator >>(std::istream& (*pf)(std::istream&));
 
 	public: // misc
 		template <typename T>
-		void prompt(const std::string & question, T & dest);
+		void prompt(const std::string & question, T & dest) {
+	    const std::lock_guard<std::mutex> lk(_mx);
+	    dest << _prefix << question;
+    }
 		void setPrefix(const std::string & prefix);
-		void flush(void);
 };
 
 extern thread_local ThreadSafeIOStream threadSafeCout;
--- src/thread_safe_iostream.cpp
+++ src/thread_safe_iostream.cpp
@@ -5,55 +5,28 @@ std::mutex ThreadSafeIOStream::_mx;
 
 void ThreadSafeIOStream::setPrefix(const std::string & prefix)
 {
+  const std::lock_guard<std::mutex> lk(_mx);
 	_prefix = prefix;
-}
-
-template <typename T>
-void ThreadSafeIOStream::prompt(const std::string & question, T & dest)
-{
-	const std::lock_guard<std::mutex> lk(_mx);
-	dest << _prefix << " ";
-	dest << question << "\n";
-}
-
-template <typename T>
-ThreadSafeIOStream & ThreadSafeIOStream::operator<<(const T & obj)
-{
-	const std::lock_guard<std::mutex> lk(_mx);
-	_buffer << obj;
-	return *this;
 }
 
 ThreadSafeIOStream & ThreadSafeIOStream::operator<<(std::ostream& (*pf)(std::ostream&))
 {
 	const std::lock_guard<std::mutex> lk(_mx);
 	pf(_buffer);
-	return *this;
-}
-
-template <typename T>
-ThreadSafeIOStream & ThreadSafeIOStream::operator>>(T & obj)
-{
-	const std::lock_guard<std::mutex> lk(_mx);
-	_buffer >> obj;
+  _flush();
 	return *this;
 }
 
 ThreadSafeIOStream & ThreadSafeIOStream::operator>>(std::istream& (*pf)(std::istream&))
 {
 	const std::lock_guard<std::mutex> lk(_mx);
-	_istream.str(_buffer.str());
-	_istream.clear(); // clear err flags
-	_istream.seekg(0, std::ios::beg); // reset read pos to start
-	pf(_istream);
+	pf(std::cin);
 	return *this;
 }
 
-void ThreadSafeIOStream::flush(void)
+void ThreadSafeIOStream::_flush(void)
 {
-	const std::lock_guard<std::mutex> lk(_mx);
-	std::cout << _prefix << " ";
-	std::cout << _buffer.str() << "\n";
+	std::cout << _prefix << _buffer.str();
 	_buffer.str("");
 	_buffer.clear();
 }
