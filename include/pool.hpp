62c30bb357b2f20865fac37c8da66126fff0b4f6
Ben Thomas <benjamin.h.thomas1@gmail.com>
2025-05-02 17:07:45 +0200

    added basic template for class initialises with size 0. Deleted copy and assignment constructors for simplicity. Added basic object subclass. Added simple resize function to edit private deque _objs. Added flawed acquire function template (will need to be replaced). Added private variables _objs and _size to monitor share_ptrs

--- include/pool.hpp
+++ include/pool.hpp
@@ -3,6 +3,7 @@
 #include <cuchar>
 #include <iostream>
 #include <memory>
+#include <deque>
 
 /*
 Manages a collection of reusable templated TType objects,
@@ -12,14 +13,33 @@ when needed, calling the destructor of the TType object but
 without deallocating the memory
 */
 
-template <typename TType> class Pool {
-private:
-public:
-  template <typename T> class Object {
-  private:
+template <typename TType>
+class Pool {
   public:
-    TType *operator->();
-  };
-  void resize(const size_t &numberOfObjectStored);
-  template <typename... TArgs> Pool::Object<TType> acquire(TArgs &&...p_args);
+    Pool() : _size(0){};
+    Pool(Pool & other) = delete; // for simplicity
+    Pool & operator=(const Pool&) = delete; // for simplicity
+
+    class Object {
+      public:
+        TType *operator->() {
+          return *this;
+        }
+    };
+
+    void resize(const size_t &numberOfObjectStored) {
+      _objs.resize(numberOfObjectStored);
+      _size = numberOfObjectStored;
+    };
+
+    template<typename ... TArgs>
+    Pool::Object<TType> acquire(TArgs&& p_args) {
+      std::shared_ptr new_obj = std::make_shared<TType>(p_args);
+      _objs.push_back(new_obj);
+      return new_obj;
+    };
+
+  private:
+    std::deque<std::shared_ptr<TType>> _objs;
+    size_t _size;
 };
