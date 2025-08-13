02efdd5ebad87d4120e3af664ece180c645f0c28
Ben Thomas <benjamin.h.thomas1@gmail.com>
2025-08-13 11:28:02 +0200

    removed useless comments added object constructor added object destructor added object shared pointer & pool pointer added fill function (need to delete this later) added pool release function (need to delete this later too)

--- include/pool.hpp
+++ include/pool.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <cuchar>
 #include <iostream>
 #include <memory>
 #include <deque>
@@ -17,29 +16,59 @@ template <typename TType>
 class Pool {
   public:
     Pool() : _size(0){};
-    Pool(Pool & other) = delete; // for simplicity
-    Pool & operator=(const Pool&) = delete; // for simplicity
+    Pool(Pool & other) = delete;
+    Pool & operator=(const Pool&) = delete;
 
     class Object {
       public:
+        Object(Pool *ptr_owner, std::shared_ptr<TType> ptr_self) :
+          _ptrOwner(ptr_owner),
+          _ptrSelf(std::move(ptr_self))
+        {
+        };
         TType *operator->() {
-          return *this;
-        }
+          return _ptr_self.get();
+        };
+        ~Object() {
+          _ptrOwner->_release(_ptrSelf);
+        };
+      private:
+        std::shared_ptr<TType> _ptrSelf;
+        Pool *_ptrOwner;
     };
 
     void resize(const size_t &numberOfObjectStored) {
       _objs.resize(numberOfObjectStored);
       _size = numberOfObjectStored;
+      _fill();
     };
 
     template<typename ... TArgs>
     Pool::Object<TType> acquire(TArgs&& p_args) {
-      std::shared_ptr new_obj = std::make_shared<TType>(p_args);
-      _objs.push_back(new_obj);
-      return new_obj;
+      if (_objs.empty()) {
+        throw std::runtime_error("Cannot acquire object from empty pool.")
+      }
+      std::shared_ptr<TType> ptr = _objs.back();
+      _objs.pop_back();
+      return ptr.get();
     };
+    void _release(std::shared_ptr<TType> item);
 
-  private:
-    std::deque<std::shared_ptr<TType>> _objs;
-    size_t _size;
+    private:
+      std::deque<std::shared_ptr<TType>> _objs;
+      size_t _size;
+      void _fill() {
+        size_t len = _objs.size();
+        if (len < _size) {
+          size_t diff = _size - len;
+          for (size_t i = 0; i < diff; i++) {
+            _objs.push_back(std::make_shared<TType>());
+          }
+        }
+      }
+};
+
+template <typename TType>
+void Pool<TType>::_release(std::shared_ptr<TType> item) {
+  _objs.push_back(std::move(item));
 };
